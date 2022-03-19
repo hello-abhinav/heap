36a1dc17a830681887d4644759544432fcb68ed1
hello-abhinav <84836820+hello-abhinav@users.noreply.github.com>
2022-03-19 23:47:33 +0530

    Create heapify.cpp

---
+++ heapify.cpp
@@ -0,0 +1,36 @@
+
+void heapify(int arr[],int n,int i)
+{
+    int largest=i;
+    int left=2*i;
+    int right=2*i+1;
+    if(left<n && arr[largest]<arr[left]){
+        largest=left;
+    }
+
+    else if(right<n &&arr[largest]<arr[right])
+    {
+        largest=right;
+    }
+    if(largest!=i)
+    {
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }
+}
+
+
+int main()
+{
+    int arr[6]={-1,54,53,55,52,50};
+    int n=5;
+    for(int i=n/2;i>0;i--)
+    {
+        heapify(arr,n,i);
+    }
+    cout<<"printing the array after heapify "<<endl;
+    for(int i=1;i<=n;i++)
+    {cout<<arr[i]<<" ";}
+    cout<<endl;
+    return 0;
+}
